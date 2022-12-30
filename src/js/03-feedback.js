0d95e3a130f6a97264a6dad75d924d08827e912f
Oksana Yerep <114570807+Oksana-y@users.noreply.github.com>
2022-12-30 17:35:59 +0200

    Fix bugs

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,15 +3,14 @@ import throttle from "lodash.throttle";
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('.feedback-form input');
 const message = document.querySelector('.feedback-form textarea');
-const formData = {};
+const formData = {email: "", message: ""};
+
+form.addEventListener('input', throttle(onForm, 500));
 
 function onForm(e) {
     formData[e.target.name] = e.target.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));  
 }
-
-form.addEventListener('input', throttle(onForm, 500));
-
 
 function getItemLs(){
     const data = JSON.parse(localStorage.getItem("feedback-form-state"));
