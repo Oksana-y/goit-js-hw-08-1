5a1f591ee6b664af6eadc5df721fe4b9478d1cf4
Oksana Yerep <114570807+Oksana-y@users.noreply.github.com>
2022-12-30 17:19:24 +0200

    fixing problems

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,25 +5,23 @@ const email = document.querySelector('.feedback-form input');
 const message = document.querySelector('.feedback-form textarea');
 const formData = {};
 
-getItemLs();
-
+function onForm(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));  
+}
 
 form.addEventListener('input', throttle(onForm, 500));
 
-function onForm(e) {
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData));    
-}
 
 function getItemLs(){
     const data = JSON.parse(localStorage.getItem("feedback-form-state"));
 
     if(data){
-     email.value = data.email;
-     message.value = data.message;
+     email.value = data.email || "";
+     message.value = data.message || "";
     }
 }
-
+getItemLs();
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
